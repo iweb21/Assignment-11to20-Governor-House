0b9a2aab572fc1c07b38f8b557c18bfea5be4b52
Sumera Imran <159164615+iweb21@users.noreply.github.com>
2024-03-07 19:26:14 +0500

    Add files via upload

---
+++ ass13.js
@@ -0,0 +1,6 @@
+"use strict";
+// your own Array:Think of your favourite mode of transportation,such as a motorcycle or a car,ana make a list that stores several examples.Use your list to print a series of statements about these items,such as a "i would like to own a Honda motorcycle."
+const  myTrans string[] = ["car", "Bike", "Bus", "Motorcycle"];
+for (let i = 0; i <= 3; i++) {
+    console.log(`I would like to own a ${myTrans[i]}`);
+}
---
+++ ass13.ts
@@ -0,0 +1,7 @@
+// your own Array:Think of your favourite mode of transportation,such as a motorcycle or a car,ana make a list that stores several examples.Use your list to print a series of statements about these items,such as a "i would like to own a Honda motorcycle."
+
+const myTrans[]: string = ["car","Bike","Bus","Motorcycle"];
+
+for(let i=0; i<=3; i++){
+    console.log(`I would like to own a ${myTrans[i]}`);
+}

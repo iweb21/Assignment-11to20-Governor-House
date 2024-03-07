67c670bb8c107a7fd7ce1836e24ba9f34aaad140
Sumera Imran <159164615+iweb21@users.noreply.github.com>
2024-03-07 19:28:46 +0500

    Add files via upload

---
+++ ass20.js
@@ -0,0 +1,7 @@
+"use strict";
+// think of something you could store in a array. for example: you could make list of mountains, rivers, countires, cities, languauges and else your would like. write a programm that creates a list containing these items.
+let proglan = ["typescript", "python", "javascript", "ruby", "c++"];
+// list of these items
+proglan.forEach((ib) => {
+    console.log(ib);
+});
---
+++ ass20.ts
@@ -0,0 +1,10 @@
+// think of something you could store in a array. for example: you could make list of mountains, rivers, countires, cities, languauges and else your would like. write a programm that creates a list containing these items.
+
+let proglan : string [] = ["typescript","python","javascript","ruby","c++"];
+
+// list of these items
+proglan.forEach((ib) => {
+   
+    console.log(ib);
+
+})

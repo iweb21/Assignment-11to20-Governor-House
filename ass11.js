8788179f9a21efc117d0c56ee4d5518738b9d7ee
Sumera Imran <159164615+iweb21@users.noreply.github.com>
2024-03-07 19:25:40 +0500

    Add files via upload

---
+++ ass11.js
@@ -0,0 +1,7 @@
+"use strict";
+// Names:Store the name of a few of your friends in a array called names.Print each person name by accessing each element in the list,one at a time.
+var names = ["Ahad", "samad", "imran", "najam"];
+console.log(names[0]);
+console.log(names[1]);
+console.log(names[2]);
+console.log(names[3]);
---
+++ ass12.ts
@@ -0,0 +1,8 @@
+// Greetings:Start with the array you used in exercise 11,but instead of just printing each person's name,print a message to them.The text of each message should be the same,but each message should be personalized with the person name.
+
+let friendName: string[] = ["ahad","bilal","samad","ali"];
+
+console.log(`Hello ${friendName[0]}, how are you?`);
+console.log(`Hello ${friendName[1]}, how are you?`);
+console.log(`Hello ${friendName[2]}, how are you?`);
+console.log(`Hello ${friendName[3]}, how are you?`);
